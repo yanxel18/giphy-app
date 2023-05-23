7b732c2507d6dc4baf1c6bf7a6bae16414783f9d
yanxel18@gmail.com <yanxel18@gmail.com>
2023-05-23 23:43:53 +0900

    update

--- src/components/c-imagethumbnail/c-imagethumbnail.component.ts
+++ src/components/c-imagethumbnail/c-imagethumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { AppService } from 'src/app/app.service';
 import { IGiphyData } from 'src/models/giphy-interface';
@@ -10,12 +10,12 @@ import { CImageviewDialogComponent } from '../c-imageview-dialog/c-imageview-dia
   styleUrls: ['./c-imagethumbnail.component.sass'],
   providers: [AppService],
 })
-export class CImagethumbnailComponent {
+export class CImagethumbnailComponent implements OnInit {
   @Input() InGIFData!: IGiphyData;
   // @Output() OutImageError = new EventEmitter<any>();
   GifNodeData!: Observable<IGiphyData>;
   
-
+  imageCaption!: string;
   constructor(private appService: AppService,
     private viewGIFDialog: MatDialog) {}
   showImage(imageData: IGiphyData): void {
@@ -29,7 +29,17 @@ export class CImagethumbnailComponent {
   checkifload(event: Event): void {
     (event.target as HTMLImageElement).style.display = 'none';
   }
- 
+  ngOnInit() {
+    this.imageCaption = this.titleShortener(
+      this.InGIFData.title === ''
+        ? this.InGIFData.username
+        : this.InGIFData.username === '' ? this.InGIFData.slug : this.InGIFData.title);
+  }
+  titleShortener(title: string): string {
+    if (title.length > 25) {
+      return title.substring(0, 25) + '...';
+    }else return title 
+  }
   loadGifNode(): void {
     this.GifNodeData = this.appService.nodeGIF(this.InGIFData.id).pipe(
       map((data) => {
--- src/components/c-imageview-dialog/c-imageview-dialog.component.ts
+++ src/components/c-imageview-dialog/c-imageview-dialog.component.ts
@@ -10,16 +10,33 @@ import { IGiphyData } from 'src/models/giphy-interface';
 export class CImageviewDialogComponent {
   gifReceiveData!: IGiphyData;
   imageCaption!: string;
+  imageOnload: boolean = false;
   constructor(
     public dialogRef: MatDialogRef<CImageviewDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public gifData: IGiphyData
   ) {
     this.gifReceiveData = gifData;
   }
-  ngOnInit() {
-    this.imageCaption =
+  loaderTagStyle: any = {
+    'background-color': '#e2e2e2',
+    height: '200px',
+    'border-radius': '0px',
+    width: '300px', 
+    margin: '0px',
+ 
+  };
+  ngOnInit(): void {
+    this.imageCaption = this.titleShortener(
       this.gifReceiveData.title === ''
         ? this.gifReceiveData.username
-        : this.gifReceiveData.title;
+        : this.gifReceiveData.username === '' ? this.gifReceiveData.slug : this.gifReceiveData.title);
+  }
+  titleShortener(title: string): string {
+    if (title.length > 25) {
+      return title.substring(0, 25) + '...';
+    }else return title 
+  }
+  checkifload(event: Event): void {
+    (event.target as HTMLImageElement).style.display = 'none';
   }
 }
